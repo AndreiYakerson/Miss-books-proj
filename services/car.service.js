1d79b2ce183131627abee541713937ebdd605a7c
AndreiYakerson <tubikwheels@gmail.com>
2025-06-16 19:35:06 +0300

    Change local storage key

--- services/car.service.js
+++ services/car.service.js
@@ -1,8 +1,8 @@
 import { loadFromStorage, makeId, saveToStorage } from './util.service.js'
 import { storageService } from './async-storage.service.js'
 
-const CAR_KEY = 'carDB'
-_createCars()
+const BOOK_KEY = 'books'
+_createBooks()
 
 export const carService = {
     query,
@@ -14,7 +14,7 @@ export const carService = {
 }
 
 function query(filterBy = {}) {
-    return storageService.query(CAR_KEY)
+    return storageService.query(BOOK_KEY)
         .then(cars => {
             if (filterBy.txt) {
                 const regExp = new RegExp(filterBy.txt, 'i')
@@ -28,19 +28,19 @@ function query(filterBy = {}) {
 }
 
 function get(carId) {
-    return storageService.get(CAR_KEY, carId)
+    return storageService.get(BOOK_KEY, carId)
 }
 
 function remove(carId) {
     // return Promise.reject('Oh No!')
-    return storageService.remove(CAR_KEY, carId)
+    return storageService.remove(BOOK_KEY, carId)
 }
 
 function save(car) {
     if (car.id) {
-        return storageService.put(CAR_KEY, car)
+        return storageService.put(BOOK_KEY, car)
     } else {
-        return storageService.post(CAR_KEY, car)
+        return storageService.post(BOOK_KEY, car)
     }
 }
 
@@ -51,21 +51,6 @@ function getEmptyCar(vendor = '', speed = '') {
 function getDefaultFilter() {
     return { txt: '', minSpeed: '' }
 }
-
-function _createCars() {
-    let cars = loadFromStorage(CAR_KEY)
-    if (!cars || !cars.length) {
-        cars = [
-            _createCar('audu', 300),
-            _createCar('fiak', 120),
-            _createCar('subali', 50),
-            _createCar('mitsu', 150)
-        ]
-        saveToStorage(CAR_KEY, cars)
-    }
-}
-
-
 
 function _createBooks() { 
     const ctgs = ['Love', 'Fiction', 'Poetry', 'Computers', 'Religion'] 
