c1bf81bf8ec90ed808413318905a159b379a5f36
AndreiYakerson <tubikwheels@gmail.com>
2025-06-17 15:54:19 +0300

    Add page count in BookDetails

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -22,15 +22,22 @@ export function BookDetails({ bookId, onBack }) {
         return amount > 200 ? 'red' : 'green'
     }
 
+    function getReading(pageCount) {
+        if (pageCount > 500) return 'Serious Reading'
+        if (pageCount > 200) return 'Descent Reading'
+        return 'Light Reading'
+    }
+
     if (!book) return <div>Loading...</div>
 
-    const { title, description, thumbnail, listPrice  } = book
+    const { title, description, thumbnail, listPrice, pageCount  } = book
 
     return (
         <section className="book-details container">
             <h1>Title: {title}</h1>
             <h1>Description: </h1>
             <p>{description}</p>
+            <p>Page count: {pageCount} ({getReading(pageCount)})</p>
             <img src={thumbnail} alt="Book Image" />
             <p>Price: <span className={getPriceClass(listPrice.amount)}>{listPrice.amount}</span></p>
             <button onClick={onBack}>Back</button>
